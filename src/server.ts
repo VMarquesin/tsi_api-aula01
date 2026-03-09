72a9f1506a516de1b399eb38e8ed72f38a06c96d
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2026-03-09 20:36:47 -0300

    Novas requisições

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.use( express.json() );
+
 app.get( "/", (req: Request, res: Response )=> {
     res.send("Olá, express!");
 
@@ -11,9 +12,19 @@ app.get( "/", (req: Request, res: Response )=> {
 
 app.get("/saudacao/:nome", (req: Request, res: Response )=>{
     const nome = req.params.nome;
-    res.send( "Olá " + nome );
+    //res.send( "Olá " + nome );
+    res.json({
+        mensagem: `Olá ${nome}!`
+    });
+});
 
-})
+app.post("/saudacao", (req: Request, res: Response) => {
+    const {nome} = req.body;
+
+    res.json({
+        mensagem: `Olá ${nome}!`
+    });
+});
 
 app.listen(PORT, () => {
 
